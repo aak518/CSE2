60058bcb4aa5fa4a8f77405717b745a9cc8ba47c
aak518 <aak518@lehigh.edu>
2015-08-28 15:59:33 +0000

    updated HelloWorld

---
+++ Lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,27 @@
+// Anas Kamal
+// Lab01 
+// Hello world Java Program
+//
+// first compile the program 
+// javac HelloWorld.java
+// run the program
+//   java HelloWorld
+
+// define a class
+public class HelloWorld{
+    
+    // add main mehtod
+    public static void main(String[] args) {
+        // print the statement with box around it
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("#                          ");
+        System.out.println("#  Hellow World 2015      #");
+        System.out.println("#                          ");
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("test");
+        
+        //test/testest
+    }
+}
