706dd3126a3312a0562f6a62e6226b748223b0c3
aak518 <aak518@lehigh.edu>
2015-08-31 21:16:51 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+// Anas Kamal
+// Lab01 
+// hw1 Java Program
+//
+// first compile the program 
+// javac WelcomeClass.java
+// run the program
+//   java WelcomeClass
+
+// define a class
+public class WelcomeClass{
+    
+    // add main mehtod
+    public static void main(String[] args) {
+        // print the statement with box around it
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-A--A--K--5--1--8->");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("v  v  v  v  v  v");
+        System.out.println("Over the summer, I went paintballing, it was really fun!");
+    }
+}
